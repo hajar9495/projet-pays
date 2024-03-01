9b3d82577abec70d4492069d1d90fbc66ebea695
hajar9495 <hajarmennai@grpgformation.onmicrosoft.com>
2024-03-01 18:05:02 +0100

    ajout de l'image

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        travel: "url('../src/assets/img/travel-img.jpg')",
+      },
+    },
   },
   plugins: [],
 };
